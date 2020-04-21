e0dc7c1b3c30e8551b4872c1dd50cd21160e573b
PrO100OleG <64087472+PrO100OleG@users.noreply.github.com>
2020-04-21 17:36:24 +0300

    Add files via upload

---
+++ proj_bibl/script.js
@@ -0,0 +1,10 @@
+function menuFun() {
+document.getElementById('navigation').classList.toggle("menuF");
+
+document.getElementById('aphone').style.opacity="1";
+document.getElementById('aphone1').style.opacity="1";
+document.getElementById('aphone2').style.opacity="1";
+document.getElementById('aphone3').style.opacity="1";
+document.getElementById('aphone4').style.opacity="1";
+document.getElementById('aphone5').style.opacity="1";
+}
